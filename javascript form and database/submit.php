d30117a030c86bbe2897a8ac91945980d2cae17b
Aadhila61 <132154720+Aadhila61@users.noreply.github.com>
2024-04-12 11:24:48 +0530

    Add files via upload

---
+++ javascript form and database/submit.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <title>Document</title>
+</head>
+<style>
+  body{
+        background-image: url(https://www.novactech.in/campaign/assets/img/education/banner.jpg);
+        /* width:max-content; */
+  }
+  #heading{
+        color: white;
+        text-align: center;
+        font-size: 20px;
+        padding-bottom: 20px;
+    }
+
+    #text-align{
+      padding-top:30px;
+        padding-left: 100px;
+        color: white;
+      }
+
+      #form{
+        padding-top:200px;
+      }
+        </style>
+<?php
+include "connection.php";
+
+  ?>
+
+<body><section id="bg-img">
+        <div class="row">
+            <div class="col-md-7" id="text">  <h1 id="text-align">Revamp the training and learning<span id="span"> Immersive Technologies</span> <br>in the fray</h1>
+            </div>
+            <div class="col-md-4">
+           
+                        <div class="row" id="form">
+                           <h2 id="text-align">"THANK YOU!  </h1>
+                           <h3 id="text-align">Form details added to database successfully"</h3>
+
+    
+  
+  </form>
+  
+</body>
+</html>
